1f8ea7230f39772c8ace1bda65a02e2cbb73c884
Igor de Souza <igordesouzadezembro19@gmail.com>
2025-06-24 06:22:42 -0300

    começando tarefas

---
+++ app/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+
+
+export default function Navbar(){
+    return(
+        <nav>
+            <div>
+            <div>
+                Logo
+            </div>
+            <div>
+                <ul>
+                    <li>Contatos</li>
+                    <li>Home</li>
+                    <li>Listab de Tarefas</li>
+                    <li>Login</li>
+                    <li>Pagina de Comentarios</li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    )
+}
